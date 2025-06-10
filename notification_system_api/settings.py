da395a9633f70802a62707f8fc78baf4613bbbb4
Igboke Daniel <danieligboke669@gmail.com>
2025-06-10 22:02:51 +0100

    Add commented MySQL database configuration to settings.py

--- notification_system_api/settings.py
+++ notification_system_api/settings.py
@@ -82,6 +82,19 @@ DATABASES = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
+    # : {
+    #     "ENGINE": "django.db.backends.mysql",
+    #     "NAME": os.getenv("MYSQL_DB"),
+    #     "USER": os.getenv("MYSQL_USER"),
+    #     "PASSWORD": os.getenv("MYSQL_PASSWORD"),
+    #     "HOST": os.getenv("MYSQL_HOST"),
+    #     "PORT": os.getenv("MYSQL_PORT"),
+    #     "OPTIONS": {
+    #         "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+    #         "charset": "utf8mb4",
+    #         "collation": "utf8mb4_unicode_ci",
+    #     },
+    # }
 }
 
 
