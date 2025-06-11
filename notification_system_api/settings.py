98ef494339b94143225e7807358d371f09b79b0b
Igboke Daniel <danieligboke669@gmail.com>
2025-06-11 22:09:51 +0100

    Update email port configuration to use a fixed value of 587

--- notification_system_api/settings.py
+++ notification_system_api/settings.py
@@ -170,7 +170,7 @@ USE_TZ = True
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
