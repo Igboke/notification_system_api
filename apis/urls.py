2e68a61b14a32aa48e8130aa14c1f32553dd31d3
Igboke Daniel <danieligboke669@gmail.com>
2025-06-11 21:56:29 +0100

    Add ArticleCreateView URL pattern for article creation

--- apis/urls.py
+++ apis/urls.py
@@ -3,9 +3,10 @@ Urls
 """
 
 from django.urls import path
-from .views import UserRegistrationView, EmailVerificationView
+from .views import UserRegistrationView, EmailVerificationView, ArticleCreateView
 
 urlpatterns = [
     path("register/", UserRegistrationView.as_view(), name="user-register"),
     path("verify-email/", EmailVerificationView.as_view(), name="verify-email"),
+    path("article/", ArticleCreateView.as_view(), name="article-create"),
 ]
